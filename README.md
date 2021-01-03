517ae9cc7a45d067728d237e5ec1f5950854a077
Mena Wahba <menawahba@gmail.com>
2021-01-03 14:15:29 -0500

    new

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ There should be no instillation involved. The link provided should take you stra
 
 If you follow the provided link in the installation section, it will take straight to the Horiseon site. The links in the navigation section will take you to the linked sections towards the lower portion of the page. There, you will find more information on these criteria's. 
 
-![](./assets/pwGenerator.JPG)
+![](assets/pwGenerator.JPG)
 
 
 ## License
--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 // Assignment Code
-  // GIVEN I need a new, secure password
-  // need a button or link to get a new password
-  // need to show the password on the page
-
-  // var generateBtn = document.querySelector("#generate");
-
+// prompts for the number of characters the password needed, looking for a number and stores it in a variable called pwLength.
   var pwLength = Number(prompt("How many characters would you like your password to be?"));
+// check pwLength and make sure its between 8 and 128 charachters, otherwise prompts to for required charactaristics. 
   while (isNaN(pwLength) || pwLength < 8 || pwLength > 128) pwLength = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
-
+// if all character lenghth is okay, prompt the following allowable characters.
   var capLetter = confirm("Would you like to use uppercase letters?");
   var lowLetter = confirm("Would you like to use lowercase letters?");
   var numbers = confirm("Would you like to use numbers?");
   var symbols = confirm("Would you like to use special characters?");
 
   while (!capLetter && !lowLetter && !numbers && !symbols) {
+    // if non of the characteristics are selected, alert to sillict at least once and cycle through them again.
     alert("You must select at least one character type!");
     capLetter = confirm("Would you like to use uppercase letters?");
     lowLetter = confirm("Would you like to use lowercase letters?");
@@ -25,62 +22,28 @@
 window.addEventListener('load', function () {
   writePassword();
 });
-  // Write password to the #password input
-      // WHEN I click the button to generate a password
-    // THEN I am presented with a series of prompts for password criteria
-       // WHEN the password is generated
-  // THEN the password is either displayed in an alert or written to the page
+//password function
   function writePassword() {
-
+// password is stored in string
     var password = "";
+    // select password ID
     var passwordText = document.querySelector("#password");
   
     var charSelected = {};
+    // if charachter catigory selected, randomly store it in charselected variable
     if (capLetter) password += rando(charSelected.capLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     if (lowLetter) password += rando(charSelected.lowLetter = "abcdefghijklmnopqrstuvwxyz");
     if (numbers) password += rando(charSelected.numbers = "1234567890");
     if (symbols) password += rando(charSelected.symbols = "!@#$%^&*(){}[]=<>/,.");
-
+    // loops as many times as pwLength and selects characterselected and stores them into password variable.
     for (var i = password.length; i < pwLength; i++) password += rando(rando(charSelected).value);
     passwordText.value = password;
-
+// replaces place holder with passowrd
     document.getElementById("password").value = randoSequence(password).join("");
   
   }
+  // an added function for refresh button to refresh page incase diffrent characteristics are needed. 
 function refresh() {
   window.location.reload("Refresh")
 }
   
-  // ************************ function here *********************************
-  // write a function called generatePassword which will have a series of prompts for user input
-  
-  // WHEN prompted for password criteria
-  // THEN I select which criteria to include in the password
-  
-  
-  // WHEN prompted for the length of the password
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-  // We have to ask the user for how many characters they want in a password
-  // we have to check to make sure it is between 8 and 128
-  // otherwise, we tell user to fix their input.
-  
-  // WHEN prompted for character types to include in the password
-  // THEN I choose lowercase, uppercase, numeric, and/or special characters
-  // confirm if they want lowercase
-  // confirm if they want uppercase
-  // confirm if they want numeric
-  // confirm if they want special characters
-  
-  // WHEN I answer each prompt
-  // THEN my input should be validated and at least one character type should be selected
-  // we need to show an error if they gave us no characters to choose from
-  
-  // WHEN all prompts are answered
-  // THEN a password is generated that matches the selected criteria
-  // from all the characters chosen, choose one randomly and add it to our password X number of times
-  // 
-  
-  // ************************ function ends here *********************************
-  
-  // Add event listener to generate button
- 
